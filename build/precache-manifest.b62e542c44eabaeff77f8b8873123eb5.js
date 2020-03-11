108c1a53a4201911b51dc61f5bb85ee8b04c0623
chriscrichter <chris_richter@outlook.com>
2020-03-11 17:23:00 -0400

    react-create-app on Heroku

---
+++ build/precache-manifest.b62e542c44eabaeff77f8b8873123eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "469ad21958a02fa78c97ea9a47b12162",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9d103e84194bc019572",
+    "url": "/static/css/main.04565166.chunk.css"
+  },
+  {
+    "revision": "96847bba62efd2a989b0",
+    "url": "/static/js/2.e73e262e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e73e262e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d103e84194bc019572",
+    "url": "/static/js/main.f70a88c3.chunk.js"
+  },
+  {
+    "revision": "4d52af317ba6f82c38b5",
+    "url": "/static/js/runtime-main.1648b397.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port, () => {
+   console.log(`Server is up on port ${port}!`);
+});
